64ad9537f0a94c533293d3435463e8f294a74ff4
JongkukLee <reevekrjoon2006@gmail.com>
2018-08-29 11:42:54 -0400

    .

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ var path = require("path");
 var HTTP_PORT = process.env.PORT || 8080;
 
 // setup the static folder 
-app.use(express.static(__dirname + "/GS1-Angular-Prototype/dist")); 
+app.use(express.static(__dirname + "/dist")); 
 
 // handle "404" errors
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname + "/GS1-Angular-Prototype/dist/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 // setup a 'route' to listen on the default url path
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
-// });
+// });ldfkldkf
 
 // Start the server
 app.listen(HTTP_PORT, function(){ 
